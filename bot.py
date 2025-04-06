39864ab9bb3cf8d09863363d0e62967cf813846c
= <=>
2025-04-06 16:41:40 -0300

    log into x and make a tweet about the speed

--- bot.py
+++ bot.py
@@ -1,8 +1,18 @@
 import time
-
+import os
+from dotenv import load_dotenv
 from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
+load_dotenv()
+
+X_EMAIL = os.getenv("X_EMAIL")
+X_USERNAME = os.getenv("X_USERNAME")
+X_PWD = os.getenv("X_PWD")
+PROMISED_DOWN = 900
+PROMISED_UP = 550
 
 class InternetSpeedTwitterBot:
     def __init__(self):
@@ -24,11 +34,46 @@ class InternetSpeedTwitterBot:
         go_button = self.driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[1]/div[1]/a/span[4]')
         go_button.click()
 
-        time.sleep(80)
-        self.down = self.driver.find_element(By.XPATH, value='//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[1]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span')
-        self.up = self.driver.find_element(By.XPATH, value='//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[1]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span')
-        print(self.down.text)
-        print(self.up.text)
+        time.sleep(70)
+        self.down = self.driver.find_element(By.XPATH, value='//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[1]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
+        self.up = self.driver.find_element(By.XPATH, value='//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[1]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
 
     def tweet_at_provider(self):
-        pass
+        self.driver.get("https://x.com/login")
+
+        time.sleep(2)
+        email = self.driver.find_element(By.XPATH, value='//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[4]/label/div/div[2]/div/input')
+
+        email.send_keys(X_EMAIL)
+        time.sleep(2)
+        email.send_keys(Keys.ENTER)
+
+        time.sleep(3)
+
+        # Check for extra username/phone input step
+        try:
+            username_input = self.driver.find_element(By.XPATH,
+                                                      '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input')
+            username_input.send_keys(X_USERNAME)  # your username/phone
+            username_input.send_keys(Keys.ENTER)
+            time.sleep(2)
+        except NoSuchElementException:
+            pass
+
+        password = self.driver.find_element(By.XPATH, value='//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')
+        password.send_keys(X_PWD)
+        time.sleep(2)
+        password.send_keys(Keys.ENTER)
+
+        time.sleep(5)
+        tweet_compose = self.driver.find_element(By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div/div/span')
+
+        tweet = f"Hey Internet Provider, why is my internet speed {self.down} down / {self.up} up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
+        tweet_compose.send_keys(tweet)
+        time.sleep(3)
+
+        tweet_button = self.driver.find_element(By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[2]/div[2]/div/div/div/button')
+        tweet_button.click()
+
+        time.sleep(2)
+        self.driver.quit()
