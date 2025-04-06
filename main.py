57ca022611ee4e72abd31c00c2481a464dfda78e
= <=>
2025-04-06 16:00:53 -0300

    removed unused imports and vars

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import os
-from dotenv import load_dotenv
 from bot import InternetSpeedTwitterBot
-
-load_dotenv()
-
-PROMISED_DOWN = 700
-PROMISED_UP = 350
-X_EMAIL = os.getenv("X_EMAIL")
-X_PWD = os.getenv("X_PWD")
-
 
 bot = InternetSpeedTwitterBot()
 bot.get_internet_speed()
