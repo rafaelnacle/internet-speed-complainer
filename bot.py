565730ab42a237d63e66609b07c3dd9e5199399b
= <=>
2025-04-06 15:31:54 -0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+class InternetSpeedTwitterBot:
+    def __init__(self, driver_path):
+        self.driver = webdriver.Chrome()
+        self.up = 0
+        self.down = 0
+
+    def get_internet_speed(self):
+        pass
+
+    def tweet_at_provider(self):
+        pass
---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+from bot import InternetSpeedTwitterBot
+
+load_dotenv()
+
+PROMISED_DOWN = 700
+PROMISED_UP = 350
+CHROME_DRIVER_PATH = os.getenv("CHROME_DRIVER_PATH")
+X_EMAIL = os.getenv("X_EMAIL")
+X_PWD = os.getenv("X_PWD")
+
+
+bot = InternetSpeedTwitterBot(CHROME_DRIVER_PATH)
+bot.get_internet_speed()
+bot.tweet_at_provider()
