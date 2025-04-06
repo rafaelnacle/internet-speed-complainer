b95537d3b3a56b7789bcf0bb907b95bd7d717a4a
= <=>
2025-04-06 15:47:18 -0300

    accept terms and press Go button

--- bot.py
+++ bot.py
@@ -1,13 +1,29 @@
+import time
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 
 class InternetSpeedTwitterBot:
-    def __init__(self, driver_path):
-        self.driver = webdriver.Chrome()
+    def __init__(self):
+        self.options = webdriver.ChromeOptions()
+        self.options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(options=self.options)
         self.up = 0
         self.down = 0
 
     def get_internet_speed(self):
-        pass
+        self.driver.get("https://www.speedtest.net/")
+
+        time.sleep(3)
+        accept_button = self.driver.find_element(By.ID, value='onetrust-accept-btn-handler')
+        accept_button.click()
+
+        time.sleep(3)
+
+        go_button = self.driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[1]/div[1]/a/span[4]')
+        go_button.click()
+
 
     def tweet_at_provider(self):
         pass
--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import os
 from dotenv import load_dotenv
-
+from selenium import webdriver
 from bot import InternetSpeedTwitterBot
 
 load_dotenv()
 
 PROMISED_DOWN = 700
 PROMISED_UP = 350
-CHROME_DRIVER_PATH = os.getenv("CHROME_DRIVER_PATH")
 X_EMAIL = os.getenv("X_EMAIL")
 X_PWD = os.getenv("X_PWD")
 
 
-bot = InternetSpeedTwitterBot(CHROME_DRIVER_PATH)
+bot = InternetSpeedTwitterBot()
 bot.get_internet_speed()
 bot.tweet_at_provider()
